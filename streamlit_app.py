00df10459834d18359f626c5884da09d34c1993b
raoulfasel93 <73660706+raoulfasel93@users.noreply.github.com>
2021-08-24 14:37:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,39 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+import ubiops 
+import tempfile
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+st.write("streamlit example")
 
-    points_per_turn = total_points / num_turns
+PROJECT_NAME = st.text_input("Project name")
+API_TOKEN = st.text_input("TOKEN",type="password")
+DEPLOYMENT_NAME = st.text_input("Deployment name")
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+api = None
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+
+
+configuration = ubiops.Configuration(host="https://api.ubiops.com/v2.1")
+configuration.api_key['Authorization'] = API_TOKEN
+
+client = ubiops.ApiClient(configuration)
+api = ubiops.CoreApi(client)
+st.write(api.service_status())
+
+upload_file = st.file_uploader("Choose a file")
+if upload_file is not None:
+    st.write(upload_file)
+    tf = tempfile.NamedTemporaryFile()
+    tf.write(upload_file.read())
+    blob = api.blobs_create(project_name=PROJECT_NAME, file=upload_file.name)
+    st.write(blob)
+    # Make a request using the blob id as input.
+    data = {'data': blob.id}
+    
+    api.deployment_version_requests_create(
+        project_name=PROJECT_NAME,
+        deployment_name=DEPLOYMENT_NAME,
+        version="v1",
+        data=data
+    )
